c941173e46e5b88350aeb911cfe7636128a9cb7c
Brittany Blake <brittany.blake49@gmail.com>
2020-06-29 17:04:40 +0200

    add ability to create and destroy sesssions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
         @current_user || User.find(session[:user_id]) if (session[:user_id])
     end
 
-    def logged_in
+    def logged_in?
         !!current_user
     end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
     def new
     end
+
+    def create
+        user = User.find_by(username:params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id 
+            flash[:success] = "You have been successfully logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Something went wrong"
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have been logged out"
+        redirect_to login_path
+    end
 end
