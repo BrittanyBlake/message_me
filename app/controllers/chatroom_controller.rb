8bcdc630e41871f7117ea70542853dee2078e44b
Brittany Blake <brittany.blake49@gmail.com>
2020-06-29 18:03:29 +0200

    Add messages from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomController < ApplicationController
     before_action :require_user, only:[:index]
     def index
+        @message = Message.new
         @messages = Message.all
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :messages
     validates :username, presence: true, length:{minimun:3, maximum:20}, uniqueness:{case_sensitive:false}
     has_secure_password
 end
