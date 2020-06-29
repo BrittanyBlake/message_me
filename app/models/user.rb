308a2fb9e5b67919ece31da2df7044e1593c0f2f
Brittany Blake <brittany.blake49@gmail.com>
2020-06-29 13:28:11 +0200

    Build message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length:{minimun:3, maximum:20}, uniqueness:true
+    validates :username, presence: true, length:{minimun:3, maximum:20}, uniqueness:{case_sensitive:false}
     has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_111216) do
+ActiveRecord::Schema.define(version: 2020_06_29_112300) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
