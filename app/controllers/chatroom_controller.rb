a126bf34aefb46b14dc05f7a03b46171f37795aa
Brittany Blake <brittany.blake49@gmail.com>
2020-07-01 12:05:08 +0200

    modify input box functionality and create custom scope

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,6 +2,6 @@ class ChatroomController < ApplicationController
     before_action :require_user, only:[:index]
     def index
         @message = Message.new
-        @messages = Message.all
+        @messages = Message.custom_display
     end
 end
